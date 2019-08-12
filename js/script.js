5cfe7ccdcd896287f887c919529aeb881b9efd45
ponlawat-w <ponlawat_w@cmu.ac.th>
2019-08-12 12:53:13 +0700

    Update Hotkeys

--- js/script.js
+++ js/script.js
@@ -1,24 +1,137 @@
 let apiBase = '';
+const numKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numKeyToIndex = val => {
+  const idx = (val - 1) % 10;
+  return idx < 0 ? idx + 10 : idx;
+};
 const catchError = (r) => { document.body.innerHTML = '<div style="text-align: center; color: #ff0000">× エラー</div>'; };
 const getErrorMessage = response => response.data && response.data.status ? response.data.status : 'Error';
+const prevent = (e) => { e.stopImmediatePropagation(); e.preventDefault(); };
+const chunkArray = (arr, size = 10) => {
+  const results = [];
+  while (arr.length) {
+    results.push(arr.splice(0, size));
+  }
+  return results;
+};
 
-const init = () => new Vue({
+const initKanjisList = templateHtml => {
+  Vue.component('kanjis-list', {
+    template: templateHtml,
+    props: [
+      'list',
+      'starttabindex',
+      'mainid'
+    ],
+    data: function() {
+      return {
+        focused: null,
+        numKeys: numKeys
+      };
+    },
+    methods: {
+      lineKey: function(e) {
+        if (this.focused === null) {
+          return;
+        }
+        const val = parseInt(e.key);
+        if (!isNaN(val)) {
+          const idx = numKeyToIndex(val);
+          if (this.list && this.list[this.focused] && this.list[this.focused][idx]) {
+            this.$emit('setsentaku', this.list[this.focused][idx]);
+          }
+          return;
+        }
+
+        switch (e.key) {
+          case 'ArrowDown':
+            this.focused++; break;
+          case 'ArrowUp':
+            this.focused--; break;
+          case 'Home':
+            this.focused = 0; break;
+          case 'End':
+            this.focused = this.list.length; break;
+          default:
+            return;
+        }
+        prevent(e);
+        if (this.focused < 0) {
+          this.focused = 0;
+        }
+        if (this.focused >= this.list.length - 1) {
+           this.focused = this.list.length - 1;
+        }
+        this.$emit('setfocus', `${this.mainid}${this.focused}`);
+      },
+      sentaku: function(k) {
+        this.$emit('setsentaku', k);
+      }
+    }
+  });
+};
+
+const initMain = () => new Vue({
   el: '#app',
   data: {
+    focusList: [],
+    numKeys: numKeys,
     jisentaku: null,
+    jisentakuFocused: false,
     kensakuKeyword: '',
     kensakuKanjis: [],
+    kensakuFocused: false,
     kensakuKanji: null,
     kensakuError: null,
     kensakuchuu: false,
+    kensakuResultFocused: false,
     kumiawaseParts: [],
     kumiawaseResults: [],
     kumiawaseError: null,
-    kumiawasechuu: false
+    kumiawasechuu: false,
+    kumiawaseFocused: false
   },
   methods: {
+    focusId: function(id) {
+      const el = document.getElementById(id);
+      if (el) {
+        el.focus();
+        return true;
+      }
+      return false;
+    },
+    focus: function(id) {
+      if (!this.focusId(id) && this.focusList.indexOf(id) === -1) {
+        this.focusList.push(id);
+      }
+    },
     sentaku: function(k) {
       this.jisentaku = k;
+      this.focus('jisentaku');
+    },
+    jisentakuKey: function(e) {
+      switch (e.key) {
+        case 'a':
+          this.addKumiawase(); this.focus('kensakuKeyword'); break;
+        case 's':
+          this.kensakuAction(); this.focus('kensakuKeyword'); break;
+        case 'j':
+          if (e.altKey) { window.open(this.jishoUrl); } else { window.location = this.jishoUrl; } break;
+        case 'Escape':
+          this.sentaku(null); this.focus('kensakuKanjis'); break;
+      }
+    },
+    kensakuKey: function(e) {
+      if (e.target.id === 'kensakuKeyword') {
+        return;
+      }
+      const val = parseInt(e.key);
+      if (!isNaN(val)) {
+        const idx = numKeyToIndex(val);
+        if (idx < this.kensakuKanjis.length) {
+          this.sentaku(this.kensakuKanjis[idx]);
+        }
+      }
     },
     kensakuAction: function() {
       if (this.jisentaku) {
@@ -42,7 +155,7 @@ const init = () => new Vue({
       }
     },
     addKumiawase: function() {
-      if (this.jisentaku) {
+      if (this.jisentaku && this.kumiawaseAddable) {
         this.kumiawaseParts.push(this.jisentaku);
         this.jisentaku = null;
       }
@@ -60,7 +173,7 @@ const init = () => new Vue({
         this.kumiawaseError = null;
         axios.get(`${apiBase}/api/v1/kanji/kumiawase/${this.kumiawaseParts.join('')}`).then(response => {
           if (response.data.ok) {
-            this.kumiawaseResults = response.data.data;
+            this.kumiawaseResults = chunkArray(response.data.data);
           } else {
             this.kumiawaseError = getErrorMessage(response);
           }
@@ -70,24 +183,114 @@ const init = () => new Vue({
           this.kumiawasechuu = false;
         });
       }
+    },
+    kumiawaseKey: function(e) {
+      switch (e.key) {
+        case 'c':
+          this.clearKumiawase();
+          break;
+        case 's':
+          this.actionKumiawase();
+          break;
+        case 'l':
+          this.focus('kumiawaseResults0');
+          break;
+      }
+    },
+    kensakuResultKey: function(e) {
+      if (!this.kensakuKanji) {
+        return;
+      }
+
+      switch (e.key) {
+        case 'k':
+          this.sentaku(this.kensakuKanji.kanji); break;
+        case 'b':
+          this.focus('minorParts0'); break;
+        case 'p':
+          this.focus('majorParts0'); break;
+      }
+    },
+    keyup: function(e) {
+      if (e.ctrlKey && e.shiftKey) {
+        switch (e.key) {
+          case 'S':
+            this.sentaku(null);
+            this.focus('kensaku');
+            break;
+          case 'K':
+            this.sentaku(null);
+            this.focus('kumiawase');
+            break;
+        }
+      } else if (e.ctrlKey) {
+        switch (e.key) {
+          case 'i':
+            this.focus('kensakuResults');
+            break;
+        }
+      }
     }
   },
   watch: {
     kensakuKeyword: function() {
       this.kensakuKanjis = this.kensakuKeyword.split('');
+    },
+    kumiawaseParts: function() {
+      this.kumiawaseResults = [];
+    }
+  },
+  computed: {
+    kumiawaseAddable: function() {
+      return this.kumiawaseParts.indexOf(this.jisentaku) === -1;
+    },
+    minorParts: function() {
+      return this.kensakuKanji.minorParts ?
+        chunkArray(this.kensakuKanji.minorParts) : [];
+    },
+    majorParts: function() {
+      return this.kensakuKanji.majorParts ?
+        chunkArray(this.kensakuKanji.majorParts) : [];
+    },
+    jishoUrl: function() {
+      return `https://jisho.org/search/${this.jisentaku}%20%23kanji`;
+    }
+  },
+  mounted: function() {
+    this.focus('kensakuKeyword');
+    document.addEventListener('keyup', this.keyup);
+  },
+  updated: function() {
+    for (let i = 0; i < this.focusList.length; i++) {
+      if (this.focusId(this.focusList[i])) {
+        this.focusList.splice(i--, 1);
+      }
     }
   }
 });
 
 window.onload = () => {
-  axios.get('/app/main.html', {
+  let kanjisListTemplate = null;
+
+  const loadMainTemplatePromise = axios.get('/app/main.html', {
     t: new Date().getTime()
   }).then(response => {
     document.body.innerHTML = response.data;
-    const app = init();
+  }).catch(r => catchError(r));
+
+  const loadKanjisListTemplatePromise = axios.get('/app/kanjis-list.html', {
+    t: new Date().getTime()
+  }).then(response => {
+    kanjisListTemplate = response.data;
+  }).catch(r => catchError(r));
+  
+  const loadApiBasePromise = axios.get('/api-base').then(response => {
+    apiBase = response.data;
   }).catch((r) => { catchError(r); });
+
+  Promise.all([loadMainTemplatePromise, loadKanjisListTemplatePromise, loadApiBasePromise]).then(() => {
+    initKanjisList(kanjisListTemplate);
+    initMain();
+  });
 };
 
-axios.get('/api-base').then(response => {
-  apiBase = response.data;
-}).catch((r) => { catchError(r); });
